23ff5c2cb4ccad479dc0b2e8be13de63fbc8f984
Muhammad Daffa' A.R <daffavcd@gmail.com>
2020-12-07 11:48:15 +0700

    buat front end chat dan table migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['online', 'away','busy','offline']);
+            $table->string('profile_picture');
             $table->rememberToken();
             $table->timestamps();
         });
