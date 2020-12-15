0621211d41f2159f1311e28efa2022b61f05c5bb
Muhammad Daffa' A.R <daffavcd@gmail.com>
2020-12-15 10:53:17 +0700

    betulkan tampilan tampil file / gambar yang masih jelek.NB:zoom saat klik gambar belum,last message masih bug saat menerima file,untuk menerima file css hover masih jelek

--- resources/views/chat/messages.blade.php
+++ resources/views/chat/messages.blade.php
@@ -1,27 +1,3 @@
-<style>
-    .file-preview {
-        border-radius: 2px;
-        transition: 0.3s;
-        max-width: 270px;
-        background: #435f7a;
-        display: inline-block;
-    }
-
-    .container-file {
-        padding: 8px 14px 8px 12px;
-        color: #f5f5f5;
-        line-height: 130%;
-    }
-
-    .file-show {
-        border-top-left-radius: 2px !important;
-        border-top-right-radius: 2px !important;
-        width: 100% !important;
-        margin: 0 !important;
-        border-radius: 0 !important;
-        float: none !important;
-    }
-</style>
 <ul>
     @foreach ($messages as $item)
     <li class="@if ($item->sender_id == Auth::id()) sent @else replies @endif">
@@ -29,7 +5,10 @@
         @if($item->type==null)
         <p>{{$item->text}}</p>
         @else
-        <div class="file-preview">
+        @if($item->type=="image")
+        <div class="file-preview" @if ($item->sender_id == Auth::id()) style="float:left !important;color: #f5f5f5;"
+            @else
+            style="float:right !important;background:#f5f5f5 !important" @endif>
             <img class="file-show" src="{{asset('storage/file/'.$item->file)}}" alt="image error" />
             @if($item->text!==null)
             <div class="container-file">
@@ -37,6 +16,18 @@
             </div>
             @endif
         </div>
+        @elseif($item->type=="file")
+        <div class="file-preview-2" @if ($item->sender_id == Auth::id()) style="float:left !important;color: #f5f5f5;"
+            @else
+            style="float:right !important;background:#f5f5f5 !important" @endif>
+            <a href="{{asset('storage/file/'.$item->file)}}" class="link-file" target="_blank">
+                <i class="fa fa-file" id="file_preview" aria-hidden="true">
+                </i>
+                <p style="padding:0px" class="link-file">&nbsp;{{$item->file}}</p>
+            </a>
+        </div>
+        @endif
+
         @endif
     </li>
     @endforeach
